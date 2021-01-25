4dedba7136e46f708bd85daf009e64bd907ef229
AntonioNarcilio <antonionarcilio@gmail.com>
2021-01-25 19:45:26 -0300

    Aula 1 finalizada

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,53 @@
 import styled from 'styled-components'
+import Widget from '../src/components/Widget'
+import db from '../db.json'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizLogo from '../src/components/QuizLogo'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground 
+      backgroundImage={db.bg} 
+      backgroundImageMobile={db.bg_mobile}>
+      <QuizContainer>
+
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Testando ..</p>
+          </Widget.Content>
+        </Widget>
+
+       <Footer />
+
+      </QuizContainer>
+
+      <GitHubCorner projectUrl="https://github.com/antonionarcilio" />
+    </QuizBackground>
+  );
 }
