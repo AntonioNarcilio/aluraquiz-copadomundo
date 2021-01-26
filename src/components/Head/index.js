cf4ea6bdbe965e116c410827c0fa2fbd2dbc3246
AntonioNarcilio <antonionarcilio@gmail.com>
2021-01-25 21:11:47 -0300

    Adicionando tag og:image

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -7,7 +7,7 @@ function HeadPage() {
 			<title>{db.title}</title>
 			<meta property="og:title" content={db.title} key="title" />
 
-			<meta property="og:image" content={db.bg} />
+			<meta property="og:image" content={db.cover_site} />
 			<meta property="og:image:type" content="image/jpeg" />
 			<meta property="og:image:width" content="800" /> 
 			<meta property="og:image:height" content="600" />
