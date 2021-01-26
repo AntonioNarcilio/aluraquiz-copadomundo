47c12416e7dc58917a1499772ae321904e5a9ecc
AntonioNarcilio <antonionarcilio@gmail.com>
2021-01-26 11:26:46 -0300

    Alterando background

--- src/components/GitHubCorner/index.js
+++ src/components/GitHubCorner/index.js
@@ -12,7 +12,7 @@ const Wrapper = styled.div`
 
 const SVGWrapper = styled.svg`
   fill: ${({ theme }) => theme.colors.secondary};
-  color: ${({ theme }) => theme.colors.contrastText};
+  color: ${({ theme }) => theme.colors.mainBg};
   cursor: pointer;
   &:hover .octo-arm{
     animation:octocat-wave 560ms ease-in-out
