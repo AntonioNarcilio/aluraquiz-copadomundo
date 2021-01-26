10c3097b3aacb46144842f525b1fcf8e8fb6531d
AntonioNarcilio <antonionarcilio@gmail.com>
2021-01-25 21:07:37 -0300

    Adicionando tags

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 import styled from 'styled-components'
 import Widget from '../src/components/Widget'
-import db from '../db.json'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
 import QuizBackground from '../src/components/QuizBackground'
 import QuizLogo from '../src/components/QuizLogo'
+import db from '../db.json'
+import HeadPage from '../src/components/Head'
+
 
 export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 70px;
   margin: auto 10%;
+
   @media screen and (max-width: 500px) {
     margin: 10% auto;
     padding: 15px;
@@ -20,34 +23,39 @@ export const QuizContainer = styled.div`
 
 export default function Home() {
   return (
-    <QuizBackground 
-      backgroundImage={db.bg} 
-      backgroundImageMobile={db.bg_mobile}>
-      <QuizContainer>
-      <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>{db.description}</p>
-          </Widget.Content>
-        </Widget>
+    <>
+      <HeadPage />
 
-        <Widget>
-          <Widget.Header>
-            <h1>Quiz</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>Em breve ...</p>
-          </Widget.Content>
-        </Widget>
+      <QuizBackground 
+        backgroundImage={db.bg} 
+        backgroundImageMobile={db.bg_mobile}>
+        <QuizContainer>
+          
+        <QuizLogo />
+          <Widget>
+            <Widget.Header>
+              <h1>{db.title}</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>{db.description}</p>
+            </Widget.Content>
+          </Widget>
 
-       <Footer />
+          <Widget>
+            <Widget.Header>
+              <h1>Quiz</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>Em breve ...</p>
+            </Widget.Content>
+          </Widget>
 
-      </QuizContainer>
+        <Footer />
 
-      <GitHubCorner projectUrl="https://github.com/antonionarcilio" />
-    </QuizBackground>
+        </QuizContainer>
+
+        <GitHubCorner projectUrl="https://github.com/antonionarcilio" />
+      </QuizBackground>
+    </>
   );
 }
---
+++ src/components/Head/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import db from '../../../db.json'
+
+function HeadPage() {
+  return (
+		<Head>
+			<title>{db.title}</title>
+			<meta property="og:title" content={db.title} key="title" />
+
+			<meta property="og:image" content={db.bg} />
+			<meta property="og:image:type" content="image/jpeg" />
+			<meta property="og:image:width" content="800" /> 
+			<meta property="og:image:height" content="600" />
+		</Head>
+  )
+}
+
+export default HeadPage
