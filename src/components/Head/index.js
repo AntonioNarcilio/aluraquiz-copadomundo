bfb93d31b62a5513773db47a846553b7eaec220a
AntonioNarcilio <antonionarcilio@gmail.com>
2021-01-25 21:29:20 -0300

    Adicionando tag og:image

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -5,12 +5,20 @@ function HeadPage() {
   return (
 		<Head>
 			<title>{db.title}</title>
+			<link rel="icon" href="../../images/favicon.png" sizes="16x16" type="image/png"></link>
+
+			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			<meta property="og:locale" content="pt_BR" />
+			<meta property="og:site_name" content={db.title} />
 			<meta property="og:title" content={db.title} key="title" />
+			<meta property="og:description" content={db.description} />
 
 			<meta property="og:image" content={db.cover_site} />
-			<meta property="og:image:type" content="image/png" />
+			<meta property="og:image:type" content="image/jpeg" />
 			<meta property="og:image:width" content="800" /> 
 			<meta property="og:image:height" content="600" />
+
+			<meta property="og:type" content="website"></meta>
 		</Head>
   )
 }
